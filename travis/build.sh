71216701b45308228ffa36489949e12f5e769e8c
Bart Brouns <bart@magnetophon.nl>
2016-07-26 09:34:06 +0200

    test lv2, not ladspa

--- travis/build.sh
+++ travis/build.sh
@@ -30,7 +30,7 @@ then
     echo "torture ladspas"
     for i in "${DSPs[@]}"
     do
-        plugin-torture --evil -d --ladspa --plugin "$i.lv2/manifest.ttl"
+        plugin-torture --evil -d --lv2 --plugin "$i.lv2/manifest.ttl"
         # plugin-torture --evil -d --ladspa --plugin "$i.so"
     done
 else
